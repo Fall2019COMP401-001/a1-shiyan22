9ecb5cafa581a3d3282600168c0434c94cd20bb7
wangshiyan <wangshiyan@ous-macbook-pro.wireless-1x.unc.edu>
2019-08-30 23:56:31 -0400

    123

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -2,13 +2,64 @@ package a1;
 
 import java.util.Scanner;
 
+import java.text.DecimalFormat;
+
 public class A1Novice {
 
 	public static void main(String[] args) {
-		
+
+
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int customer = scan.nextInt();
+		String FirstName = "";
+		char F = 0;
+		String LastName = "";
+		int item = 0;
+		int quantity = 0;
+		String itemname = "";
+		double price = 0;
+		double total = 0;
+		double sum = 0;
+		DecimalFormat df = new DecimalFormat("#0.00");
+
+		for(int i=0; i < customer; i++) {
+
+			FirstName = scan.next();
+
+			F = FirstName.charAt(0);
+
+			LastName = scan.next();
+
+			item = scan.nextInt();
+
+			for (int a =0; a < item; a++) {
+
+				quantity = scan.nextInt();
+
+				itemname = scan.next();
+
+				price = scan.nextDouble();
+
+				total = quantity * price;
+				
+				sum += total;
+
+
+
+			}
+			
+			System.out.println(F + ". " + LastName + ": " + df.format(sum));
+			
+			sum = 0;
 		
+
+		
+
+
+		}
+
+
+
 	}
 }

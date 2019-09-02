cf92d72017fb3b26422fda4955569a1fe7688dbf
wangshiyan <wangshiyan@ous-macbook-pro.wireless-1x.unc.edu>
2019-09-01 23:30:01 -0400

    oh yeah

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -14,18 +14,20 @@ public class A1Jedi {
 		double[] price = new double[itemtype];
 		int[] customerpitem = new int[itemtype];
 		int[] itemtotal = new int[itemtype];
+		boolean[] s = new boolean[itemtype];
 		for (int i=0; i<itemtype; i++) {
 			itemname[i] = scan.next();
 			price[i] = scan.nextDouble();
-		    customerpitem[i] = 0;
-		    itemtotal[i] = 0;
+			customerpitem[i] = 0;
+			itemtotal[i] = 0;
+			s[i] = false;
 		}
-		
+
 		int customer = scan.nextInt();
 		String[] FirstName = new String[customer];
 		String[] LastName = new String[customer];
 		double[] costpcus = new double[customer];
-		
+		int in = 0;
 		for (int a=0; a<customer; a++) {
 			FirstName[a] = scan.next();
 			LastName[a] = scan.next();
@@ -37,18 +39,25 @@ public class A1Jedi {
 			for (int b=0; b<bought; b++) {
 				typenumber[b] = scan.nextInt();
 				type[b] = scan.next();
-				costptype[b] = typenumber[b] * price[Arrays.asList(itemname).indexOf(type[b])];
+				in = Arrays.asList(itemname).indexOf(type[b]);
+				costptype[b] = typenumber[b] * price[in];
 				costpcus[a] += costptype[b];
-				customerpitem[Arrays.asList(itemname).indexOf(type[b])] ++;
-				itemtotal[Arrays.asList(itemname).indexOf(type[b])] += typenumber[b];
+				if(!s[in]) {
+					customerpitem[in] ++;
+					s[in] = true;
+				}
+				itemtotal[in] += typenumber[b];
+			}
+			for (int i=0; i<itemtype; i++) {
+				s[i] = false;
 			}
 		}
 		for (int i=0; i<itemtype; i++) {
 			if (customerpitem[i] == 0) {
 				System.out.println("No customers bought " + itemname[i]);
-				}
+			}
 			else {
-					System.out.println(customerpitem[i] + " customers bought " + itemtotal[i] + " " +itemname[i]);
+				System.out.println(customerpitem[i] + " customers bought " + itemtotal[i] + " " +itemname[i]);
 			}
 		}
 	}
